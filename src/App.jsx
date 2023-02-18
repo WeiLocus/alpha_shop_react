2d089e3a1f62ed1e9b6f17bbbe9e32a7cc316860
WeiLocus <marsuo52@gmail.com>
2023-02-18 23:17:41 +0800

    create footer block

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import styles from "styles/main.scss"
 import Header from "components/Header/Header"
 import Main from "components/Main/Main"
+import Footer from "components/Footer/Footer"
 
 function App() {
   return (
     <>
       <Header />
       <Main /> 
+      <Footer />
     </>
   )
 }
